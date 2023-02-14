ac2ce0a84f24a12c168a854806def98448c4e04d
srnazng <srnazng@gmail.com>
2023-02-14 01:18:04 -0500

    repeated a*

--- assignment1/Heap.py
+++ assignment1/Heap.py
@@ -1,76 +1,79 @@
 import sys
 
 class MinHeap:
-    def __init__(self, maxsize, compare):
-        self.maxsize = maxsize
-        self.size = 0
-        self.Heap = [0]*(self.maxsize + 1)
-        self.Heap[0] = -1 * sys.maxsize
-        self.FRONT = 1
+	def __init__(self, maxsize, compare):
+		self.maxsize = maxsize
+		self.size = 0
+		self.Heap = [0]*(self.maxsize + 1)
+		self.Heap[0] = -1 * sys.maxsize
+		self.FRONT = 1
+		self.compare = compare
 
-    # Function: return position of parent for node at pos
-    def parent(self, pos):
-        return pos//2
+	# Function: return position of parent for node at pos
+	def parent(self, pos):
+		return pos//2
 
-    # Function: return position of left child of node at pos
-    def leftChild(self, pos):
-        return 2 * pos
+	# Function: return position of left child of node at pos
+	def leftChild(self, pos):
+		return 2 * pos
 
-    # Function: return position of right child of node at pos
-    def rightChild(self, pos):
-        return (2 * pos) + 1
+	# Function: return position of right child of node at pos
+	def rightChild(self, pos):
+		return (2 * pos) + 1
 
-    # Function: returns true if passed node is leaf node
-    def isLeaf(self, pos):
-        return pos*2 > self.size
+	# Function: returns true if passed node is leaf node
+	def isLeaf(self, pos):
+		return pos*2 > self.size
 
-    # Function: swap two nodes
-    def swap(self, fpos, spos):
-        self.Heap[fpos], self.Heap[spos] = self.Heap[spos], self.Heap[fpos]
+	# Function: swap two nodes
+	def swap(self, fpos, spos):
+		self.Heap[fpos], self.Heap[spos] = self.Heap[spos], self.Heap[fpos]
 
-    # Function: heapify node at pos
-    def minHeapify(self, pos):
-        # If node is non-leaf node + > than any of children
-        if not self.isLeaf(pos):
-            if compare(self.Heap[pos], self.Heap[self.leftChild(pos)]) > 0 or compare(self.Heap[pos], self.Heap[self.rightChild(pos)]) > 0: #if pos > left or right child
-                # Swap with left child and heapify left child
-                if compare(self.Heap[self.leftChild(pos)], self.Heap[self.rightChild(pos)]) < 0: #left < right
-                    self.swap(pos, self.leftChild(pos))
-                    self.minHeapify(self.leftChild(pos))
-                # Swap with right child and heapify right child
-                else:
-                    self.swap(pos, self.rightChild(pos))
-                    self.minHeapify(self.rightChild(pos))
+	# Function: heapify node at pos
+	def minHeapify(self, pos):
+		# If node is non-leaf node + > than any of children
+		if not self.isLeaf(pos):
+			if self.compare(self.Heap[pos], self.Heap[self.leftChild(pos)]) > 0 or self.compare(self.Heap[pos], self.Heap[self.rightChild(pos)]) > 0: #if pos > left or right child
+				# Swap with left child and heapify left child
+				if self.compare(self.Heap[self.leftChild(pos)], self.Heap[self.rightChild(pos)]) < 0: #left < right
+					self.swap(pos, self.leftChild(pos))
+					self.minHeapify(self.leftChild(pos))
+				# Swap with right child and heapify right child
+				else:
+					self.swap(pos, self.rightChild(pos))
+					self.minHeapify(self.rightChild(pos))
 
-    # Function: insert node into heap
-    def insert(self, element):
-        if self.size >= self.maxsize :
-            return
-        self.size += 1
-        self.Heap[self.size] = element
-        current = self.size
-        while compare(self.Heap[current], self.Heap[self.parent(current)]) < 0: #if current < parent of current 
-            self.swap(current, self.parent(current))
-            current = self.parent(current)
+	# Function: insert node into heap
+	def insert(self, element):
+		if self.size >= self.maxsize:
+			return
+		self.size += 1
+		self.Heap[self.size] = element
+		current = self.size
+		
+		# TODO: CHECK IF self.parent(current) > 0 IS RIGHT
+		while self.parent(current) > 0 and self.compare(self.Heap[current], self.Heap[self.parent(current)]) < 0: #if current < parent of current 
+			self.swap(current, self.parent(current))
+			current = self.parent(current)
 
-    # Function: print heap
-    def Print(self):
-        for i in range(1, (self.size//2)+1):
-            print(" PARENT : " + str(self.Heap[i]) + " LEFT CHILD : " + str(self.Heap[2 * i]) + " RIGHT CHILD : " +
-                str(self.Heap[2 * i + 1]))
+	# Function: print heap
+	def Print(self):
+		for i in range(1, (self.size//2)+1):
+			print(" PARENT : " + str(self.Heap[i]) + " LEFT CHILD : " + str(self.Heap[2 * i]) + " RIGHT CHILD : " +
+				str(self.Heap[2 * i + 1]))
 
-    # Function: build min heap using minHeapify
-    def minHeap(self):
-        for pos in range(self.size//2, 0, -1):
-            self.minHeapify(pos)
+	# Function: build min heap using minHeapify
+	def minHeap(self):
+		for pos in range(self.size//2, 0, -1):
+			self.minHeapify(pos)
 
-    # Function: remove minimum element
-    def remove(self):
-        self.Heap[self.FRONT] = self.Heap[self.size]
-        self.size-= 1
-        self.minHeapify(self.FRONT)
+	# Function: remove minimum element
+	def remove(self):
+		self.Heap[self.FRONT] = self.Heap[self.size]
+		self.size = self.size - 1
+		self.minHeapify(self.FRONT)
 
-    # Function: return minimum element
-    def returnMin(self):
-        minim=self.Heap[self.FRONT]
-        return minim
+	# Function: return minimum element
+	def returnMin(self):
+		minim=self.Heap[self.FRONT]
+		return minim
--- assignment1/main.py
+++ assignment1/main.py
@@ -14,18 +14,26 @@ class Run:
         self.node_grid = [[Node(i, j) for j in range(size)] for i in range(size)] # 2d Array of Nodes with currently known values
         self.start = self.node_grid[self.gridworld.start[0]][self.gridworld.start[1]]
         self.target = self.node_grid[self.gridworld.target[0]][self.gridworld.target[1]]
-        self.print_nodes()
 
         # initialize h values for each node
         for row in self.node_grid:
             for node in row:
                 node.h = self.h(node)
 
+        self.a_star()
+
+    def a_star(self):
+        path = self.repeated_forward()
+        print("Final Map")
+        self.print_nodes()
+        print("PATH")
+        print(path)
+
     def print_nodes(self):
         for r in self.node_grid:
             for elem in r:
                 if self.f(elem) < sys.maxsize/2:
-                    print('(' + str(elem.x) + ',' + str(elem.y) + '): ' + str(self.f(elem)), end=' ') #(row, column)
+                    print('(' + str(elem.x) + ',' + str(elem.y) + '): ' + str(self.f(elem)) + '=' + str(elem.g) + '+' + str(elem.h), end=' ') #(row, column)
                 else:
                     print('(' + str(elem.x) + ',' + str(elem.y) + '): ' + str(-1), end=' ') #(row, column)
                 if elem.blocked:
@@ -48,8 +56,11 @@ class Run:
 
     def repeated_forward(self):
         final_path = []
+        
         counter = 0
         current_start = self.start
+        final_path.append(current_start)
+
         while True:
             # if current start is target then return final path
             if current_start == self.target:
@@ -59,25 +70,36 @@ class Run:
             path = self.forward(current_start, counter)
 
             # if forward returns None --> there is no path and return None
-            if self.forward() is None:
+            if path is None:
                 return None 
 
             # update counter
             counter += 1
 
-            # while not at dead end
-                # check if next step in path is valid by expanding current start
-                # if valid update map (and update search value to each node it encounters)
-            for i in (len(path) - 1):
-                current_start = path[0]
-                
+            # for each node in path 
+            for i in range(len(path) - 2):    
+                # expand current start
+                for i in range(4):
+                    x = current_start.x + dx[i]
+                    y = current_start.y + dy[i]
 
-            # if not valid step --> rerun forward() with same start
+                    if x < 0 or y < 0 or x >= self.size or y >= self.size or self.gridworld[x][y] == '#':
+                        # if blocked update map
+                        self.node_grid[x][y].blocked = True
 
-            # else --> update final_path and update current start
+                # check if reach dead end update final_path if not dead end
+                if path[i + 1].blocked:
+                    break
+                else:
+                    final_path.append(path[i + 1])
+                    current_start = path[i + 1]
+
+        return final_path
             
     # Single instance of A* on current map
     def forward(self, current_start, counter):
+        print("A*", counter)
+        self.print_nodes()
         # Search A to T
         flag = False # If G has been found
 
@@ -92,12 +114,12 @@ class Run:
         
         # Insert starting node of A* as the first node
         open_list.insert(current_start)
-
         
         # find shortest path
-        while self.target.g > (self.open_list.returnMin().g + self.open_list.returnMin().h):
+        while self.target.g > (open_list.returnMin().g + open_list.returnMin().h):
             # get node with smallest f, this node gets EXPANDED.
-            min_node = self.MinHeap.remove()
+            min_node = open_list.returnMin()
+            open_list.remove()
 
             # target found
             if min_node == self.target:
@@ -127,18 +149,20 @@ class Run:
                 if min_node.g + 1 < adj_node.g:
                     adj_node.g = min_node.g + 1
                     adj_node.parent = min_node
-                    self.MinHeap.insert(adj_node)
+                    open_list.insert(adj_node)
 
         # no path to the goal was found, so return with nothing
+        print(f"flag: {flag}")
         if not flag:
             return None
 
         # trace backwards from target to start
         path = []
         curr_node = self.target
-        while curr_node is not current_start:
+        while not curr_node == current_start:
             path.append(curr_node)
             curr_node = curr_node.parent
+        path.append(current_start)
             
         return path[::-1]
 
@@ -146,13 +170,11 @@ class Run:
         temp = self.target
         self.target = self.start
         self.start = temp
-        return repeated_forward()
+        return self.repeated_forward()
 
     def f(self, node):
-        if node.h < 0:
-            node.h = self.h(node)
         if node.g >= 0:
             return node.g + node.h
         return -1
 
-Run(10)
+Run(3)
