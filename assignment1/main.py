a0f868cea719bd821d30a21f390cacf2107c8083
srnazng <srnazng@gmail.com>
2023-02-18 16:23:33 -0500

    backward a star

--- assignment1/main.py
+++ assignment1/main.py
@@ -7,7 +7,7 @@ dx = [-1, 0, 0, 1] # boundaries of x values
 dy = [0, 1, -1, 0] # boundaries of y values
 
 class Run:
-    def __init__(self, size):
+    def __init__(self, size, forward):
         self.size = size
         self.gridworld = GridWorld(size)
         self.gridworld.print_grid()
@@ -20,10 +20,10 @@ class Run:
             for node in row:
                 node.h = self.h(node)
 
-        self.a_star()
+        self.a_star(forward)
 
-    def a_star(self):
-        path = self.repeated_forward()
+    def a_star(self, forward):
+        path = self.repeated_a_star(forward)
         if path is None:
             print("I cannot reach the target")
         else:
@@ -52,7 +52,12 @@ class Run:
             print()
 
     def compare(self, n1, n2):
-        return self.f(n1) - self.f(n2)
+        #in favor of larger g values
+        return (self.size**2*self.f(n1) - n1.g)-(self.size**2*self.f(n2)-n2.g)
+
+    def compare_smaller(self, n1, n2):
+        #in favor of smaller g values
+        return (self.size**2*self.f(n1) - n1.g)-(self.size**2*self.f(n2)-n2.g)
 
     def h(self, node):
         return self.manhattan(node, self.target)
@@ -60,7 +65,7 @@ class Run:
     def manhattan(self, a, b):
         return abs(a.x - b.x) + abs(a.y - b.y)
 
-    def repeated_forward(self):
+    def repeated_a_star(self, forward):
         final_path = []
         
         counter = 0
@@ -73,7 +78,10 @@ class Run:
                 return final_path
             
             # run A* on current map
-            path = self.forward(current_start, counter)
+            if forward:
+                path = self.forward(current_start, counter)
+            else:
+                path = self.backward(current_start, counter)
 
             # if forward returns None --> there is no path and return None
             if path is None:
@@ -104,6 +112,70 @@ class Run:
             counter += 1
 
         return final_path
+
+    # Single instance of A* on current map
+    def backward(self, current_start, counter):
+        # Search A to T
+        flag = False # If G has been found
+
+        # Create new open list (min heap)
+        open_list = MinHeap(4 * self.size * self.size, self.compare)
+
+        # Update g values of start and target
+        self.target.g = 0
+        self.target.search = counter
+        current_start.g = sys.maxsize/2
+        current_start.search = counter
+        
+        # Insert starting node of A* as the first node
+        open_list.insert(self.target)
+        
+        # find shortest path
+        while open_list.size > 0 and current_start.g > (open_list.returnMin().g + open_list.returnMin().h):
+            # get node with smallest f, this node gets EXPANDED.
+            min_node = open_list.returnMin()
+            open_list.remove()
+
+            # print(f"Popping: {min_node.x}, {min_node.y}. Target: {self.target.x}, {self.target.y}")
+            
+            # FIND SHORTEST PATH USING CURRENT KNOWLEDGE
+            # Loop through all the neighbors of the node. 
+            # If the neighbor node isn't blocked with current knowledge, check to see if the path cost (g) through the expanded node
+            # is less than the current path cost in the neighbor node. If it is, update the path cost of the node, and 
+            # insert it into the heap
+            for i in range(4):
+                x = min_node.x + dx[i]
+                y = min_node.y + dy[i]
+
+                # ensure valid unblocked node
+                if x < 0 or y < 0 or x >= self.size or y >= self.size or self.node_grid[x][y].blocked:
+                    continue
+                adj_node = self.node_grid[x][y]
+
+                # update search number of adj_node
+                if adj_node.search < counter:
+                    adj_node.g = sys.maxsize/2
+                    adj_node.search = counter
+                
+                # update cost of adj_node and insert into open list
+                if min_node.g + 1 < adj_node.g:
+                    adj_node.g = min_node.g + 1
+                    adj_node.parent = min_node
+                    open_list.insert(adj_node)
+
+        # no path to the goal was found if open list is exhausted, so return with nothing
+        if open_list.size == 0:
+            return None
+
+        # trace backwards from target to start
+        path = []
+        curr_node = current_start
+        while not curr_node == self.target:
+            path.append(curr_node)
+            curr_node = curr_node.parent
+        path.append(self.target)
+    
+        return path
             
     # Single instance of A* on current map
     def forward(self, current_start, counter):
@@ -114,7 +186,7 @@ class Run:
         open_list = MinHeap(4 * self.size * self.size, self.compare)
 
         # Update g values of start and target
-        self.target.g = sys.maxsize
+        self.target.g = sys.maxsize/2
         self.target.search = counter
         current_start.g = 0
         current_start.search = counter
@@ -169,15 +241,9 @@ class Run:
             
         return path[::-1]
 
-    def repeated_backward(self):
-        temp = self.target
-        self.target = self.start
-        self.start = temp
-        return self.repeated_forward()
-
     def f(self, node):
         if node.g >= 0:
             return node.g + node.h
         return -1
 
-Run(101)
+Run(101, False)
